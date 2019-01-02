2c4f79e3e63bca0254462f0d3e1bee9997415d4c
mseddon <matt@scryetek.com>
2019-01-02 13:06:12 +0000

    Work on Netlification

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'www')
   },
   devServer: {
     historyApiFallback: true,
